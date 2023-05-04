f9a086bdf7d4cf3e43d72017bb0171288c93b614
exam <exam@Bcalab4-m18>
2023-05-04 12:57:13 +0530

    exam

---
+++ src/casetoolexam2/zakriya_243.java
@@ -0,0 +1,15 @@
+package casetoolexam2;
+
+public class zakriya_243 {
+	int length;
+	zakriya_243 (int length)
+	{
+		this.length=length;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
